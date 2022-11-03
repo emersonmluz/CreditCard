d07f5d6039c453837301efe59481c453202e9189
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-11-03 16:30:07 -0300

    Adicionado cornerRadius

--- creditCard/creditCard/ViewController.swift
+++ creditCard/creditCard/ViewController.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerCard: UIView!
+    
+    @IBOutlet weak var gradient: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        containerCard.layer.cornerRadius = 10
+        
+        gradient.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
